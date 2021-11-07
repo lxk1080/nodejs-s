f732fdcdeb07f2d6015e93cf723aeb916729587d
unknown <2664276518@qq.com>
2021-11-07 17:57:12 +0800

    add 404

--- 12_art-demo/routes/index.js
+++ 12_art-demo/routes/index.js
@@ -8,4 +8,9 @@ const router = express.Router();
 router.use('/', mainRouter);
 router.use('/user', userRouter);
 
+// 处理 404 的情况，在未匹配到任何路由的情况下，将会进入这个中间件
+router.use(function (req, res) {
+  res.send('404 not found');
+});
+
 module.exports = router;
