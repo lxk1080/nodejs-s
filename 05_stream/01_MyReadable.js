b35d7f109824ffc4322134084a36119298eb719e
unknown <2664276518@qq.com>
2022-08-17 23:51:06 +0800

    feat: add file_read_stream

--- 05_stream/01_MyReadable.js
+++ 05_stream/01_MyReadable.js
@@ -7,11 +7,13 @@
  *
  *    - 底层的数据在不断的 _read 进缓冲区（重要！）
  *      - 从测试来看，当为可读流绑定事件时，才会不断的 _read，只是实例化并不会
+ *      - 不断的 _read 数据进缓冲区，储存量存在限度，默认是 16 KB，也就是 16384 Byte，可以通过 this.readableHighWaterMark 查看
  *
  *    - readable：当流中存在可读取数据时触发（暂停模式 - 需要通过 read 方法拿到数据）
  *      - 当缓冲区中有数据了（从无到有），就会触发 readable 事件
+ *      - 当数据被全部读完时为 null，也会触发 readable 事件
  *      - read：从缓冲区中读取数据，可以设置参数指定读多少个字节，如果没有数据可供读取，则返回 null
- *        - 当使用 read 方法时，这个时候可能已经 _read 过好多次了
+ *        - 当走到 read 方法时，这个时候可能已经 _read 过好多次了
  *
  *    - data：当流中数据块传给消费者后触发（流动模式 - 不需要通过 read 方法就能自动获得数据）
  *      - 消费者拿数据，可能不经过缓冲区
@@ -32,9 +34,9 @@ class MyReadable extends Readable {
   // 重写 _read 方法
   _read(size) {
     // super._read(size)
-    console.log('-- _read --')
     const data = this.source.shift() || null
     this.push(data)
+    console.log('-- _read --', data)
   }
 }
 
@@ -48,6 +50,7 @@ const mr = new MyReadable(source)
 // 1、readable 下面写了三种方式，帮助理解 readable 和 read() 都干了什么
 // mr.on('readable', () => {
 //   let data = null
+//   console.log('fire-readable-!!!')
 //
 //   // 1. 只要有数据就读出来（除了刚开始数据不定，后面是每 _read 一次就能读一次）
 //   // while ((data = mr.read()) !== null) {
---
+++ 05_stream/05_file_read_stream.js
@@ -0,0 +1,73 @@
+
+/**
+ * 说明：文件可读流详解
+ */
+
+const fs = require('fs')
+
+/**
+ * 1、创建可读流
+ *    - flags：文件系统标志，默认：r
+ *    - encoding：字符编码，默认：null，代表 buffer，二进制数据类型（以 16 进制表示）
+ *    - fd：文件描述符，默认：null，则自动会以 3 开始，0、1、2 已经提前被标准输入、输出、错误占用了
+ *    - mode：文件权限，默认：rw-rw-rw-，八进制 0o666，十进制 438
+ *    - autoClose：是否自动关闭文件
+ *    - start：文件读取开始位置，单位：字节
+ *    - end：文件读取结束位置（包括结束位置）
+ *    - highWaterMark：水位线，代表缓冲区最多储存多少容量的数据，单位：字节
+ *      - 在 Readable 中默认 16kb
+ *      - 在当前的文件可读流中默认 64kb
+ */
+
+const rs = fs.createReadStream('./helper/test.txt', {
+  flags: 'r',
+  encoding: null,
+  fd: null,
+  mode: 438,
+  autoClose: true,
+  start: 0,
+  end: 9,
+  highWaterMark: 3,
+})
+
+/**
+ * 2、on('data') 流动模式
+ *    - 使用 pause() 暂停流动
+ *    - 使用 resume() 恢复流动
+ */
+
+// rs.on('data', (chunk) => {
+//   console.log(chunk.toString())
+//   rs.pause()
+//   setTimeout(() => {
+//     rs.resume()
+//   }, 500)
+// })
+
+/**
+ * 3、on('readable')
+ *    - 缓冲区的数据准备好了，可以读了，就会触发 readable 事件，需要调用 read() 方法主动去读
+ *    - rs._readableState.length 缓冲区中剩余可读字节的长度
+ *    - 流程：
+ *      - 可读流向缓冲区存入数据，大小为 highWaterMark 的值，存储完毕触发 readable 事件（从无到有式触发）
+ *      - rs 使用 read() 从缓冲区读取数据，每次读取 1 个字节，输出字节和剩余可读字节的长度
+ *      - 很快，缓冲区中的数据被消耗完，但 rs 还在读，这时候，可读流再次向缓冲区存入数据
+ *      - 当数据存入好后，再次触发 readable 事件，rs 继续从缓冲区读取数据
+ *      - 重复上述步骤，直到将来的某个时刻，可读流中全部数据都给了缓冲区，缓冲区中的数据也被消耗完了
+ *      - 此时，rs 仍然在喋喋不休的 read() 数据
+ *      - 可读流还是像往常一样给缓冲区数据，不知道给了什么，它已经一无所有了，给完了之后依然触发 readable 事件（null式触发）
+ *      - rs 又来继续 read() 了，可是这一次，read 的结果为 null
+ *      - rs 结束了自己罪恶的一生
+ *
+ *      > rs 就是可读流，可读流就是 rs，受害者是一开始的数据，rs 丧心病狂将数据大卸八块，然后一口一口吃掉
+ *
+ */
+
+rs.on('readable', () => {
+  console.log('data-ready-!!!')
+  let data = null
+  while((data = rs.read(1)) !== null) {
+    console.log(data.toString())
+    console.log('------->', rs._readableState.length)
+  }
+})
